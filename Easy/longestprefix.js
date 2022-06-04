9e272699cbc9b9b8173d06c2540ccee19b02e2e0
Kevin2422 <99225907+Kevin2422@users.noreply.github.com>
2022-06-03 20:38:53 -0400

    prefix

---
+++ Easy/longestprefix.js
@@ -0,0 +1,16 @@
+var longestCommonPrefix = function(strs) {
+    let prefix = ""
+    let index = 0
+    while(index<strs[0].length){
+
+        let char = strs[0][index]
+        for(let i = 1; i<strs.length; i++){
+            if(strs[i][index]!== char){
+                return prefix
+            }
+        }
+        prefix += char
+        index++
+    }
+    return prefix
+}
